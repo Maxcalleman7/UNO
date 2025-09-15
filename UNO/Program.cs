b5649e8cf0a06757e6a4e9bcfb0f8674ed8e9af2
alle07 <al07.post@gmail.com>
2025-09-15 12:48:03 +0200

    Flyttade Playercreate till StartNewGame

--- UNO/Program.cs
+++ UNO/Program.cs
@@ -6,6 +6,12 @@ class Program
     public static void StartNewGame()
 
     {
+        PlayerHandler handler = new PlayerHandler();
+        int numPlayers = handler.NumberOfPlayers();
+
+
+        handler.CreatePlayers(numPlayers);
+
         CardPile pile = new CardPile();
         pile.CreateNewCardPile();
         pile.ShuffleCards();
@@ -15,11 +21,7 @@ class Program
         Console.WriteLine("PLAY UNO!");
         StartNewGame();
 
-        PlayerHandler handler = new PlayerHandler();
-        int numPlayers = handler.NumberOfPlayers();
 
-        
-        handler.CreatePlayers(numPlayers);
 
     }
 }
